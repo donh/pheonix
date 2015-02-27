2a51d62bab6da24bf4a585d9935664302f7380d4
Don <donh@github.com>
2015-02-27 22:43:03 +0800

    Add routes/index.js

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+/*
+ * GET home page.
+ */
+exports.root = function(req, res) {
+	res.render('phx/home', { title: 'PHX' });
+};
+
+exports.react = function(req, res) {
+	res.render('141022_react/home', { title: 'React' });
+};
+
+exports.auto = function(req, res) {
+	res.render('140811_auto/home', { title: 'Testing and Automation' });
+};
+
+exports.doc = function(req, res) {
+	res.render('140610_doc/home', { title: 'Documentation' });
+};
+
+exports.wd = function(req, res) {
+	res.render('140415_wd/home', { title: 'WD' });
+};
+
+exports.epic = function(req, res) {
+	res.render('140217_epic/home', { title: 'EPIC' });
+};
